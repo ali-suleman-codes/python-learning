7b199c823974d7f7f7958aae78437cf5f9e65d44
mALI-cpp-oops <alisuleman.cs@gmail.com>
2026-07-14 17:12:59 +0500

    feat: complete practice 01 demonstrating arithmetic operators with integer variables

---
+++ cisco-python-essentials-1/module_2_basics/sec_2_3_practice/practice_01_arithmetic_operations.py
@@ -0,0 +1,26 @@
+# Problem Statement
+
+# Create a program that demonstrates all arithmetic operators using two variables.
+
+# Requirements
+# Store two integer values in variables.
+# Display:
+# Addition
+# Subtraction
+# Multiplication
+# Division
+# Floor Division
+# Modulus
+# Exponentiation
+# Print each result with a clear label.
+
+num1 = 3
+num2 = 2
+
+print("Addition       :", num1 + num2)
+print("Subtraction    :", num1 - num2)
+print("Multiplication :", num1 * num2)
+print("Division       :", num1 / num2)
+print("Floor division :", num1 // num2)
+print("Modulus        :", num1 % num2)
+print("Exponentiation :", num1 ** num2)
