41a73e82e3b8441771fb493970608f608181383e
mALI-cpp-oops <alisuleman.cs@gmail.com>
2026-07-14 18:00:34 +0500

    refactor: rename module 2 folder using consistent numbering format

---
+++ cisco-python-essentials-1/module_02_basics/sec_2_3_practice/practice_03_expression_evaluator.py
@@ -0,0 +1,19 @@
+# Problem Statement
+
+# A scientist is calculating different formulas using fixed values.
+
+# Requirements
+
+# Create variables such as:
+
+a = 8
+b = 3
+c = 2
+
+# Calculate different mathematical expressions using these variables.
+
+print("a + b * c   =", a + b * c)
+print("a // b ** c =", a // b ** c)
+print("a ** b ** c =", a ** b ** c)
+print("a * (b + c) =", a * (b + c))
+print("a / b - c   =", a / b - c)
