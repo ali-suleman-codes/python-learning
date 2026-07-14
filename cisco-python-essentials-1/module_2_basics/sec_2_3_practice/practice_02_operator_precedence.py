fab52c211d5d9350c0a6e74725cc552a05e24cb2
mALI-cpp-oops <alisuleman.cs@gmail.com>
2026-07-14 17:50:56 +0500

    feat : complete practice 02 evaluating operator precedence in arithmetic expressions

---
+++ cisco-python-essentials-1/module_2_basics/sec_2_3_practice/practice_02_operator_precedence.py
@@ -0,0 +1,21 @@
+# Problem Statement
+
+# Create a program that demonstrates how operator precedence affects mathematical expressions.
+
+# Requirements
+
+# Calculate and display the results of expressions such as:
+
+# 5 + 2 * 3
+# (5 + 2) * 3
+# 20 / 5 + 2
+# 20 / (5 + 2)
+# 2 ** 3 * 2
+
+# Display each expression and its result.
+
+print("5 + 2 * 3    =", 5 + 2 * 3)
+print("(5 + 2) * 3  =", (5 + 2) * 3)
+print("20 / 5 + 2   =", 20 / 5 + 2)
+print("20 / (5 + 2) =", 20 / (5 + 2))
+print("2 ** 3 * 2   =", 2 ** 3 * 2)
