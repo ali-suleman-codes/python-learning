10140e22280f231b652d94763c43df78563240e2
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-15 11:02:15 +0500

    refactor : clean up comments, rename variables and complete output logic in lab 2.5.3

---
+++ cisco-python-essentials-1/module_02_basics/sec_2_5_comments/lab_2_5_3_comments.py
@@ -0,0 +1,31 @@
+# Scenario
+# The code in the editor contains comments. Try to improve it: add or remove comments where you find it appropriate (yes, sometimes removing a comment can make the code more readable), and change variable names where you think this will improve code comprehension.
+
+# original code in the editor
+print("Original code : " )
+
+#this program computes the number of seconds in a given number of hours
+# this program has been written two days ago
+
+a = 2 # number of hours
+seconds = 3600 # number of seconds in 1 hour
+
+print("Hours: ", a) #printing the number of hours
+# print("Seconds in Hours: ", a * seconds) # printing the number of seconds in a given number of hours
+
+#here we should also print "Goodbye", but a programmer didn't have time to write any code
+#this is the end of the program that computes the number of seconds in 3 hour
+
+
+
+# Updated version 
+print("Updated version : ")
+
+# this program computes the number of seconds in a given number of hours
+
+hours = 2 
+seconds_per_hour = 3600 
+
+print("Number of hours :",hours) 
+print("Seconds in",hours,"hours :", hours * seconds_per_hour) 
+print("Goodbye")
