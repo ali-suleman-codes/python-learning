388b717b71d8e0f818e5b6a9767fe205fd33e681
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-15 23:00:45 +0500

    feat : complete lab 3.1.11 if else statement

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_1_decision_making/lab_3_1_11_if_else_statement.py
@@ -0,0 +1,23 @@
+# Scenario
+# Once upon a time there was a land – a land of milk and honey, inhabited by happy and prosperous people. The people paid taxes, of course – their happiness had limits. The most important tax, called the Personal Income Tax (PIT for short) had to be paid once a year, and was evaluated using the following rule:
+
+# if the citizen's income was not higher than 85,528 thalers, the tax was equal to 18% of the income minus 556 thalers and 2 cents (this was what they called tax relief)
+# if the income was higher than this amount, the tax was equal to 14,839 thalers and 2 cents, plus 32% of the surplus over 85,528 thalers.
+# Your task is to write a tax calculator.
+
+# It should accept one floating-point value: the income.
+# Next, it should print the calculated tax, rounded to full thalers. There's a function named round() which will do the rounding for you – you'll find it in the skeleton code in the editor.
+# Note: this happy country never returned any money to its citizens. If the calculated tax was less than zero, it would only mean no tax at all (the tax was equal to zero). Take this into consideration during your calculations.
+
+income = float(input("Enter income : "))
+tax = 0
+if income <= 85528 :
+    tax = (income * 18 / 100) - 556.2
+elif income > 85528 :
+    tax = (income - 85528) * 32 / 100 + 14839.2
+
+tax = round(tax)
+if tax < 0 :
+    tax = 0
+
+print("Personal income tax :", tax)
