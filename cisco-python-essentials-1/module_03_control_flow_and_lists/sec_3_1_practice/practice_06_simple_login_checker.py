9ca34447d74ed292591ea54ea82d29c8ad08cb4b
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-15 23:56:25 +0500

    feat : complete practice 06 simple login checker

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_1_practice/practice_06_simple_login_checker.py
@@ -0,0 +1,21 @@
+# Goal
+
+# Ask for a username.
+
+# If username is
+
+# admin
+
+# display
+
+# Access Granted
+
+# otherwise
+
+# Access Denied
+
+user_name = input("Enter username : ")
+if user_name == "admin" :
+    print("Access granted")
+else :
+    print("Access denied")
