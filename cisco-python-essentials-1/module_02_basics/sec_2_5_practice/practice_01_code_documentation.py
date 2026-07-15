aa796466d33091aa72f83aad3614cfb74597a081
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-15 11:34:33 +0500

    feat : complete practice 01 documenting python code using comments

---
+++ cisco-python-essentials-1/module_02_basics/sec_2_5_practice/practice_01_code_documentation.py
@@ -0,0 +1,32 @@
+# Create a program that demonstrates the proper use of comments in Python.
+
+# Requirements
+
+# • Add a title comment describing the program.
+# • Create variables for:
+#     - Student Name
+#     - Course
+#     - Age
+# • Print the stored information.
+# • Calculate the student's age after five years and display it.
+# • Add at least one commented-out print() statement that does not execute.
+# • The program output should not display any comments.
+
+# Student information system 
+print("============== Student Information ==============\n")
+
+student_name = "Muhammad Ali Suleman Rajpoot"
+course_title = "Python Essentials 1"
+age = 20
+
+print("Student name       :", student_name)
+print("Course title       :", course_title)
+print("Age                :", age)
+
+future_age = age + 5
+print("Age after 5  years :", future_age)
+print("\n============================== ==================")
+
+# print("This line is commented out")
+
+
