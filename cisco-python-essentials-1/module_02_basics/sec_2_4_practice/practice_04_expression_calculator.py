a8b87164bf8139455c638e453a86b532726074af
mALI-cpp-oops <alisuleman.cs@gmail.com>
2026-07-15 09:51:45 +0500

    feat : complete practice 04 evaluating mathematical expressions into new variables

---
+++ cisco-python-essentials-1/module_02_basics/sec_2_4_practice/practice_04_expression_calculator.py
@@ -0,0 +1,29 @@
+# Problem Statement
+
+# A scientist is calculating different mathematical formulas.
+
+# Requirements
+
+# Create variables:
+
+a = 10
+b = 4
+c = 3
+
+# Store the results of several expressions in new variables.
+
+# Use:
+
+# Parentheses
+# Arithmetic operators
+# Exponentiation
+# Floor division
+# Modulus
+
+# Display all calculated results with proper labels.
+
+expression_one = (a ** b) * c
+expression_two = c // (a % b)
+
+print("Result of Exponentiation and Multiplication :", expression_one)
+print("Result of Modulus and Floor Division :", expression_two)
