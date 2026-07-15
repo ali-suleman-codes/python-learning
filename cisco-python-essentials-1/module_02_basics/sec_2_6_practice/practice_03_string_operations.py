5bd328828a6edcc53f2bdf7f841ab4a5734005ab
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-15 18:08:03 +0500

    feat : complete practice 03 demonstrating string operators with user input

---
+++ cisco-python-essentials-1/module_02_basics/sec_2_6_practice/practice_03_string_operations.py
@@ -0,0 +1,19 @@
+# Problem Statement
+
+# Ask the user to enter:
+
+# Their favorite word
+
+# Display:
+
+# Word repeated using *
+# Greeting using string concatenation
+# Word inside decorative borders
+
+fav_word = input("Enter your favourite word my love : ")
+
+print((fav_word + " ")* 5)
+
+print("Hi " + "my love. " + "Your favourite word is", fav_word)
+
+print("|" + fav_word + "|")
