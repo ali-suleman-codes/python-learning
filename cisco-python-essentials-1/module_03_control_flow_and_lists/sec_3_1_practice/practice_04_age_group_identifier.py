5196ea8d3aac9515ae17c61d9b8f31261a2ab699
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-15 23:54:34 +0500

    feat : complete practice 04 age group identifier

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_1_practice/practice_04_age_group_identifier.py
@@ -0,0 +1,20 @@
+# Goal
+
+# Input age.
+
+# Display
+
+# Child
+# Teenager
+# Adult
+# Senior
+
+age = int(input("Enter age : "))
+if age <= 12 :
+    print("Child")
+elif age <= 20 :
+    print("Teenager")
+elif age <= 64 :
+    print("Adult")
+else :
+    print("Senior")
