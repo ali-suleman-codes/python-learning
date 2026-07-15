0b47fe1836f93a3db960fa4b8faba81ec060462d
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-15 23:53:46 +0500

    feat : complete practice 03 pass fail checker

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_1_practice/practice_03_pass_fail_checker.py
@@ -0,0 +1,19 @@
+# Goal
+
+# Input marks.
+
+# Example:
+
+# >=50
+
+# Pass
+
+# Otherwise
+
+# Fail
+
+marks = int(input("Enter marks : "))
+if marks >= 50 :
+    print("Pass")
+else :
+    print("Fail")
