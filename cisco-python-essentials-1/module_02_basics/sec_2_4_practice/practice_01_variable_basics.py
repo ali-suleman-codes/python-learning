4c37f06d1626192edd7dd052f5f90e94873715eb
mALI-cpp-oops <alisuleman.cs@gmail.com>
2026-07-15 09:46:02 +0500

    feat : complete practice 01 storing and displaying student profile variables

---
+++ cisco-python-essentials-1/module_02_basics/sec_2_4_practice/practice_01_variable_basics.py
@@ -0,0 +1,21 @@
+# Problem Statement
+
+# Create a program that stores and displays information using variables.
+
+# Requirements
+# Create variables for:
+# Student name
+# University
+# Semester
+# Programming language
+# Display each value with proper labels.
+
+student_name = "Ali Suleman"
+university = "University of Narowal"
+semester = 2
+programming_language = "Python"
+
+print("Student name:", student_name)
+print("University name:", university)
+print("Current semester :", semester)
+print("Programming language :", programming_language)
