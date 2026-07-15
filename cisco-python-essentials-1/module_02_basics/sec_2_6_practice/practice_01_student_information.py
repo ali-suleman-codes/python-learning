4ff9d96e97f003f4bc671aacb978a61e9604587b
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-15 18:05:22 +0500

    feat : complete practice 01 collecting and displaying user information

---
+++ cisco-python-essentials-1/module_02_basics/sec_2_6_practice/practice_01_student_information.py
@@ -0,0 +1,18 @@
+# Problem Statement
+
+# Create a program that asks the user for:
+
+# Name
+# Age
+# University
+
+# Display the information in a formatted report.
+
+name = input("Enter your name : ")
+age = int(input("Enter your age : "))
+uni_name = input("Enter your university name : ")
+
+print("\n===== Student Information =====")
+print("Name       :", name)
+print("Age        :", age)
+print("University :", uni_name)
