c10a1d3fd706bd37b5051a4feb940da9205b5816
mALI-cpp-oops <alisuleman.cs@gmail.com>
2026-07-15 09:48:11 +0500

    feat : complete practice 02 variable reassignment

---
+++ cisco-python-essentials-1/module_02_basics/sec_2_4_practice/practice_02_variable_reassignment.py
@@ -0,0 +1,23 @@
+# Problem Statement
+
+# Create a program that demonstrates updating an existing variable.
+
+# Requirements
+# Create a variable named temperature.
+# Assign an initial value.
+# Display it.
+# Change the value three times using normal assignment (=).
+# Display the value after each change.
+
+temperature = 23
+print("Initial temperature :", temperature)
+
+temperature = 35
+print("Upadated temperature :", temperature)
+
+temperature = 21
+print("Upadated temperature :", temperature)
+
+temperature = 40
+print("Upadated temperature :", temperature)
+
