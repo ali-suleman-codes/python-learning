5c8447d65bece8cd5ce1d642da81a02625299a88
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-15 18:10:13 +0500

    feat : complete practice 05 converting temperatures using user input

---
+++ cisco-python-essentials-1/module_02_basics/sec_2_6_practice/practice_05_temperature_converter.py
@@ -0,0 +1,15 @@
+# Problem Statement
+
+# Ask the user to enter a temperature in Celsius.
+
+# Convert it to Fahrenheit
+# Display both temperatures
+
+# F = C × 9 / 5 + 32
+
+temp_celsius = float(input("Enter temperature in celsius : "))
+temp_fahren = temp_celsius * (9 / 5) + 32
+
+print("Temperature(Celsius)    :", temp_celsius)
+print("Temperature(Fahrenheit) :", temp_fahren)
+
