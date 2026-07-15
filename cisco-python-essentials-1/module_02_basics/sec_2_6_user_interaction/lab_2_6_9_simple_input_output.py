68c925776fe3872fabbefa0546fee92fff232258
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-15 13:15:31 +0500

    feat : complete lab 2.6.9 simple input, output and perform 4 basic arithmetic operations.

---
+++ cisco-python-essentials-1/module_02_basics/sec_2_6_user_interaction/lab_2_6_9_simple_input_output.py
@@ -0,0 +1,20 @@
+# Scenario
+# Your task is to complete the code in order to evaluate the results of four basic arithmetic operations.
+
+# The results have to be printed to the console.
+
+# You may not be able to protect the code from a user who wants to divide by zero. That's okay, don't worry about it for now.
+
+# Test your code ‒ does it produce the results you expect?
+
+# We won't show you any test data ‒ that would be too simple.
+
+num1 = float(input("Enter 1st number :"))
+num2 = float(input("Enter 2nd number :"))
+
+print("Addition of two numbers :", num1  + num2)
+print("Subtraction of two numbers :", num1  - num2)
+print("Multiplication of two numbers :", num1  * num2)
+print("Division of two numbers :", num1  / num2)
+
+print("\nThat's all, folks!")
