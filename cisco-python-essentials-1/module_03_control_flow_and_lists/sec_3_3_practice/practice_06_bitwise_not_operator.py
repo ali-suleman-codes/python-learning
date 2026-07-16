d2e4397de23ab6e13e340d5c0125ee4f434a542e
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-16 19:00:34 +0500

    feat : complete practice 06 performing bitwise not operation

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_3_practice/practice_06_bitwise_not_operator.py
@@ -0,0 +1,11 @@
+# Goal
+# Input one integer.
+# Display:
+# Original Number
+# Bitwise NOT Result
+# using ~.
+
+num = int(input("Enter a number : "))
+not_num = ~num                       
+print("Original number :", num)      
+print("Bitwise NOT result :", not_num)
