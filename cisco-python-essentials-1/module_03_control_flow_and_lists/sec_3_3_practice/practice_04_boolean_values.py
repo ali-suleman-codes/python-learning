c04a3d3b47bd4c04f493d38f8edc4969e4cfd7e9
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-16 18:58:45 +0500

    feat : complete practice 04 demonstrating boolean values and conditional execution

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_3_practice/practice_04_boolean_values.py
@@ -0,0 +1,22 @@
+# Goal
+# Store Boolean values in variables.
+# Display:
+# True
+# False
+# and use them inside simple if statements.
+
+student = True
+intelligent = False
+
+print("Student :", student)
+print("Intelligent :", intelligent)
+
+if student:
+    print("The person is a student")
+else :
+    print("The person is not a student")
+
+if intelligent:
+    print("The person is intelligent")
+else :
+    print("The person is not intelligent")
