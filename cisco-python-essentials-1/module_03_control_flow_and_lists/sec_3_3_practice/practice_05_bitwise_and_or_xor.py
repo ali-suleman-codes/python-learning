d82b9e93d242b270f7fa6910bb5b95884338603c
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-16 18:59:43 +0500

    feat : complete practice 05 performing bitwise and or xor operations

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_3_practice/practice_05_bitwise_and_or_xor.py
@@ -0,0 +1,18 @@
+# Goal
+# Create two integers.
+# Display:
+# Bitwise AND
+# Bitwise OR
+# Bitwise XOR
+
+num1 = int(input("Enter 1st integer : "))
+num2 = int(input("Enter 2nd integer : "))
+
+bitwise_and = num1 & num2
+bitwise_or = num1 | num2
+bitwise_xor = num1 ^ num2
+
+
+print("Result of bitwise AND operation :", bitwise_and)
+print("Result of bitwise OR operation :", bitwise_or)
+print("Result of bitwise XOR operation :", bitwise_xor)
