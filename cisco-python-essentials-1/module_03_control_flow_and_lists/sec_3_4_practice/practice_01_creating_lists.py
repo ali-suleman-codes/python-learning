c6eedaa646237539488db8c95e9f844c83572900
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-16 23:02:16 +0500

    feat: complete practice 01 creating and accessing lists

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_4_practice/practice_01_creating_lists.py
@@ -0,0 +1,16 @@
+# Statement
+# A game stores information about a player.
+# Create a list containing
+# player name
+# level
+# health
+# alive status
+# Print
+# the whole list
+# first element
+# last element
+
+info_player = ["King", 3, 100, True]
+print(info_player)
+print("Name  :", info_player[0])
+print("Alive :", info_player[3])
