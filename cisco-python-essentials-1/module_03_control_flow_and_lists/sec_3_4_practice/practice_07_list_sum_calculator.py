b88ea8d48aafa0311b34185f000022feaa0188c3
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-16 23:10:20 +0500

    feat: complete practice 07 calculating totals from list values

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_4_practice/practice_07_list_sum_calculator.py
@@ -0,0 +1,20 @@
+# Statement
+# A teacher records student marks.
+# Store
+# 75
+# 82
+# 91
+# 68
+# 84
+# Calculate the total marks using a loop.
+
+student_marks = [75, 82, 91, 68, 84]
+
+total = 0
+
+for marks in student_marks :
+    total += marks
+
+print("Marks       :", student_marks)
+
+print("Total marks :", total)
