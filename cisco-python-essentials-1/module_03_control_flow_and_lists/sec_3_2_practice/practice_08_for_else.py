9a62cbe1962f78ae1f88c22621c0c18817a8bad1
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-16 10:58:23 +0500

    feat : complete practice 08 using else branch of for loop

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_2_practice/practice_08_for_else.py
@@ -0,0 +1,11 @@
+# Goal
+# Print numbers 1–5 using:
+# for
+# ...
+# else
+# Display a completion message from the else block.
+
+for num in range(1, 6) :
+    print(num)
+else :
+    print("Loop ends successfully")
