3801198a4dd38fe7d98a756ce8bf951c72b7775e
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-16 23:08:23 +0500

    feat: complete practice 04 deleting list elements and checking length

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_4_practice/practice_04_list_length_and_delete.py
@@ -0,0 +1,13 @@
+# Statement
+# A classroom has the following roll numbers.
+# [101,102,103,104,105]
+# Remove the third student.
+# Print
+# updated list
+# total students
+
+roll_numbers = [101,102,103,104,105]
+del roll_numbers[2]
+
+print("Updated list :", roll_numbers)
+print("Students     :", len(roll_numbers))
