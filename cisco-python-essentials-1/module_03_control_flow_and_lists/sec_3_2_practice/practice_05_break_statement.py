4ce762653d65f0c4364efd9871daae95f12e491e
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-16 10:44:50 +0500

    feat : complete practice 05 break statement in while loop

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_2_practice/practice_05_break_statement.py
@@ -0,0 +1,10 @@
+# Goal
+# Keep asking the user for numbers.
+# Stop when the user enters:
+# 0
+# Use break.
+
+while True :
+    num = int(input("Enter a number or enter 0 for exit : "))
+    if num == 0 :
+        break
