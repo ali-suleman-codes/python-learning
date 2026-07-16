285a31e2dcfca48babce582ae222bf4eed37ca32
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-16 23:07:52 +0500

    feat: complete practice 03 updating list elements

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_4_practice/practice_03_updating_lists.py
@@ -0,0 +1,16 @@
+# Statement
+# An online store keeps product prices.
+# Create a list
+# [250, 400, 600, 800]
+# Update
+# first price to 300
+# last price to 900
+# Print list before and after updating.
+
+price_list = [250, 400, 600, 800]
+print("Before :", price_list)
+
+price_list[0] = 300
+price_list[-1] = 900
+
+print("After  :", price_list)
