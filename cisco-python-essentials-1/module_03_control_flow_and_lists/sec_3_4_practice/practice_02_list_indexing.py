6180706cf060f3a1b38e125b37090784fc411e2e
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-16 23:02:50 +0500

    feat: complete practice 02 exploring positive and negative indexing

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_4_practice/practice_02_list_indexing.py
@@ -0,0 +1,19 @@
+# Statement
+# A bookshelf contains five books.
+# Store the following books inside a list.
+# Python
+# C++
+# Java
+# SQL
+# Linux
+# Print
+# first book
+# third book
+# last book
+# second last book
+
+bookshelf = ["Python", "C++", "Java", "SQL", "Linux"]
+print("First book       :", bookshelf[0])
+print("Third book       :", bookshelf[2])
+print("Last book        :", bookshelf[-1])
+print("Second last book :", bookshelf[-2])
