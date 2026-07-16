e539c4eb526937d13ffce5acc1344fcd2a55a4e0
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-16 10:52:10 +0500

    feat : complete practice 07 using else branch of while loop

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_2_practice/practice_07_while_else.py
@@ -0,0 +1,13 @@
+# Goal
+# Count from 1 to 5 using:
+# while
+# ...
+# else
+# Display a message after the loop completes normally.
+
+num = 1 
+while num <= 5 :
+    print(num)
+    num += 1 
+else : 
+    print("Loop ends successfully")
