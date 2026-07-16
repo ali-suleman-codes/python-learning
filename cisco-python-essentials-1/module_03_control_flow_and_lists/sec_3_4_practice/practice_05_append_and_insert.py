314a12ee5c0f4004739d589dd8c7f026e81bdb4c
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-16 23:08:55 +0500

    feat: complete practice 05 adding elements with append and insert

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_4_practice/practice_05_append_and_insert.py
@@ -0,0 +1,20 @@
+# Statement
+# A football team announces players one by one.
+# Start with an empty list.
+# Add
+# Ali
+# Ahmed
+# Bilal
+# using append().
+# Insert
+# Captain
+# at the beginning.
+
+football_team = []
+football_team.append("Ali")
+football_team.append("Ahmed")
+football_team.append("Bilal")
+
+football_team.insert(0, "Captain")
+
+print(football_team)
