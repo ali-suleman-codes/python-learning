7a34b65803d3afefe10499c63033d2bf6a79364e
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-16 10:34:55 +0500

    feat : complete practice 02 sum of first 10 numbers using while loop

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_2_practice/practice_02_sum_of_first_ten_numbers.py
@@ -0,0 +1,10 @@
+# Use a while loop to calculate:
+
+# 1 + 2 + 3 + ... + 10
+
+total_sum = 0
+num = 1
+while num <= 10 :
+    total_sum += num
+    num += 1
+print("Total sum of first 10 numbers :", total_sum)
