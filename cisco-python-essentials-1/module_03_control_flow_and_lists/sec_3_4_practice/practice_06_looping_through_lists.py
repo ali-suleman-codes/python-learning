c75799a1b95b982cc75d3f320000825f1e9b8a08
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-16 23:09:48 +0500

    feat: complete practice 06 iterating through lists using for loops

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_4_practice/practice_06_looping_through_lists.py
@@ -0,0 +1,12 @@
+# Statement
+# A grocery shop has these fruits.
+# Apple
+# Banana
+# Orange
+# Mango
+# Print every fruit on a separate line using a loop.
+
+grocery_shop = ["Apple", "Banana", "Orange", "Mango"]
+
+for fruit in grocery_shop:
+    print(fruit)
