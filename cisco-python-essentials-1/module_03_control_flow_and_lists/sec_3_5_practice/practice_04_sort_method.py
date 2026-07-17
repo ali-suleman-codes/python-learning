f7fdb0b20f37e4c53037e6a7bee2880415dc3c09
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-17 10:06:21 +0500

    feat : complete practice 04 sorting list using sort method

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_5_practice/practice_04_sort_method.py
@@ -0,0 +1,11 @@
+# Statement
+# A school stores student roll numbers.
+# [108, 101, 115, 103, 110]
+# Use the built-in sort() method to arrange the roll numbers in ascending order.
+
+student_roll_numbers = [108, 101, 115, 103, 110]
+print("Original roll numbers :", student_roll_numbers)
+
+student_roll_numbers.sort()
+print("Sorted roll numbers   :", student_roll_numbers)
+
