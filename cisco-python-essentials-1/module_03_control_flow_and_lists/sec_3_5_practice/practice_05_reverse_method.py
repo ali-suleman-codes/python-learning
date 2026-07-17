80275e0cdd98b13f44bbe06f92c4588ba04f7956
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-17 10:07:12 +0500

    feat : complete practice 05 reversing the list using reverse method

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_5_practice/practice_05_reverse_method.py
@@ -0,0 +1,20 @@
+# Statement
+# A museum displays historical eras in chronological order.
+# ["Stone Age",
+#  "Bronze Age",
+#  "Iron Age",
+#  "Middle Ages",
+#  "Modern Age"]
+
+# Use the reverse() method to display them in reverse order.
+
+eras = ["Stone Age",
+ "Bronze Age",
+ "Iron Age",
+ "Middle Ages",
+ "Modern Age"]
+
+print("Original order :", eras)
+
+eras.reverse()
+print("Reverse order  :", eras)
