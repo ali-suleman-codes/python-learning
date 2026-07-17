65cdc269878277db60d13f4bc8bc66268674eedf
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-17 15:48:10 +0500

    feat : complete practice 05 extracting list sections with slicing

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_6_practice/practice_05_extract_student_marks.py
@@ -0,0 +1,14 @@
+# Statement
+# A teacher records marks.
+# [78, 65, 91, 82, 70, 88]
+# Create
+# first half
+# second half
+# Print both.
+
+marks = [78, 65, 91, 82, 70, 88]
+first_half_marks = marks[ : 3]
+second_half_marks = marks[3 : ]
+
+print("First half  :", first_half_marks)
+print("Second half :", second_half_marks)
