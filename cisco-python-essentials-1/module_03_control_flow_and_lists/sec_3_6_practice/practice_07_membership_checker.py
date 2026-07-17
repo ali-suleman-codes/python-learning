e0374520ed2f053e0e7b971e6d8b4570678bb085
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-17 15:51:09 +0500

    feat : complete practice 07 checking list membership with in and not in

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_6_practice/practice_07_membership_checker.py
@@ -0,0 +1,20 @@
+# Statement
+# A grocery shop has
+# ["Apple", "Banana", "Orange", "Mango"]
+# Check whether
+# Banana
+# Grapes
+# exist in the list.
+# Print suitable messages.
+
+fruit_list = ["Apple", "Banana", "Orange", "Mango"]
+
+if "Banana" in fruit_list:
+    print("Hurrah! Banana is available")
+else :
+    print("Alas! Banana is not available")
+
+if "Grapes" in fruit_list :
+    print("Hurrah! Grapes are available")
+else : 
+    print("Alas! Grapes are not available")
