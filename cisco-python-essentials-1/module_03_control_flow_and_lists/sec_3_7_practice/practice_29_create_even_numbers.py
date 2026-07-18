3f526d3611ebc355a9c5011593b0284e773293e5
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-18 13:34:15 +0500

    feat: complete practice 29 creating even numbers with list comprehensions

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_7_practice/practice_29_create_even_numbers.py
@@ -0,0 +1,11 @@
+# Create Even Numbers
+
+# Purpose:
+# Generate even numbers using list comprehension.
+
+# Concepts:
+# - List comprehension
+# - if condition
+
+even_numbers = [number for number in range(1, 21) if number % 2  == 0 ]
+print(even_numbers)
