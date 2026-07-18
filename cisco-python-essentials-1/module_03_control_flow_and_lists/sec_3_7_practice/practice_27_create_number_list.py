1d47b3782ce24b88b20932ca9d787bd447dba22f
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-18 13:30:07 +0500

    feat: complete practice 27 creating number lists with list comprehensions

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_7_practice/practice_27_create_number_list.py
@@ -0,0 +1,11 @@
+# Create Number List
+
+# Purpose:
+# Create a list of numbers using list comprehension.
+
+# Concepts:
+# - List comprehension
+# - range()
+
+number_list = [number for number in range(1, 11)]
+print(number_list)
