3346ee8fe5dad70c52ff6b539c321e54d8d0947d
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-18 13:31:01 +0500

    feat: complete practice 28 generating square numbers with list comprehensions

---
+++ cisco-python-essentials-1/module_03_control_flow_and_lists/sec_3_7_practice/practice_28_create_square_numbers.py
@@ -0,0 +1,11 @@
+# Create Square Numbers
+
+# Purpose:
+# Generate square numbers using list comprehension.
+
+# Concepts:
+# - List comprehension
+# - Arithmetic operations
+
+square_list = [number * number for number in range (1, 11)]
+print(square_list)
