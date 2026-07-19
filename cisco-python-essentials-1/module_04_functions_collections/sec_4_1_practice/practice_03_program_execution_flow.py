c8eb9d8a6b594271fe0c4cec270d0da5d6b4b5f8
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-19 18:40:23 +0500

    feat: complete practice 03 understanding function execution flow

---
+++ cisco-python-essentials-1/module_04_functions_collections/sec_4_1_practice/practice_03_program_execution_flow.py
@@ -0,0 +1,16 @@
+# Program Execution Flow
+
+# Purpose:
+# Understand how Python jumps into a function and returns.
+
+# Concepts:
+# - Function execution
+# - Program flow
+# - Function call
+
+def process():
+    print("Processing...")
+
+print("Program Started")
+process()
+print("Program finished")
