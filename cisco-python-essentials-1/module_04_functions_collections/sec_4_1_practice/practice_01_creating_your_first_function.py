2b0ddf27db2bcea875c2c4db3e3d39e0a6e1ee80
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-19 18:38:36 +0500

    feat: complete practice 01 creating your first user defined function

---
+++ cisco-python-essentials-1/module_04_functions_collections/sec_4_1_practice/practice_01_creating_your_first_function.py
@@ -0,0 +1,18 @@
+# Creating Your First Function
+
+# Purpose:
+# Learn how to define and call a simple function.
+
+# Concepts:
+# - def
+# - Function body
+# - Function call
+# - Indentation
+
+def message():
+    print("Welcome")
+
+
+message()
+message()
+message()
