8aee8b99bd9e97a645f30ff1de18a1e24cea6147
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-19 20:42:41 +0500

    feat: complete practice 01 creating a parameterized welcome message

---
+++ cisco-python-essentials-1/module_04_functions_collections/sec_4_2_practice/practice_01_parameterized_welcome_message.py
@@ -0,0 +1,16 @@
+# Parameterized Welcome Message
+
+# Purpose:
+# Learn how to define a function with one parameter and pass an argument during function invocation.
+
+# Concepts:
+# - One parameter
+# - Argument passing
+# - Function invocation
+
+def welcome(name):
+    print("Welcome, ", name + "!")
+
+welcome("Ali")
+welcome("Azeem")
+welcome("King")
