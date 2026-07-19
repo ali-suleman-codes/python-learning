0f4ce15b9b73045b74d24db817a8f52b0b0eb87e
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-19 20:43:17 +0500

    feat: complete practice 02 displaying student information with multiple parameters

---
+++ cisco-python-essentials-1/module_04_functions_collections/sec_4_2_practice/practice_02_student_information_display.py
@@ -0,0 +1,17 @@
+# Student Information Display
+
+# Purpose:
+# Practice creating functions with multiple parameters.
+
+# Concepts:
+# - Multiple parameters
+# - Positional arguments
+# - Function parameters
+
+
+def student_info(name, roll_no):
+    print("Student: ", name, " | Roll No :", roll_no)
+
+student_info("Ali", 1234)
+student_info("Azeem", 3455)
+student_info("King", 8739)
