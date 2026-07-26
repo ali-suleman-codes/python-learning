b7dc295259f238086772313f32db045fdd845268
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-26 18:46:36 +0500

    feat : complete practice 07 wildcard module import

---
+++ cisco-python-essentials-2/module_01_modules/sec_1_1_practice/practice_07_import_all_demo.py
@@ -0,0 +1,9 @@
+from math import *
+
+print("PI               :", pi)
+print("Square root of 5 :", sqrt(5))
+print("sin(pi / 2)      :", sin(pi / 2))
+print("Factorial of 5   :", factorial(5)) 
+
+
+# This method of importing all entities of module is very dangerous because of naming conflicts in code's namespace
