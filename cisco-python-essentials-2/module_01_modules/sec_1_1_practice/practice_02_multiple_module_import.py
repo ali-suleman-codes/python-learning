f55473323a32d33d10d328d40e1275a918d9b1f5
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-26 18:41:32 +0500

    feat : complete practice 02 using multiple modules

---
+++ cisco-python-essentials-2/module_01_modules/sec_1_1_practice/practice_02_multiple_module_import.py
@@ -0,0 +1,8 @@
+import random
+import math
+
+random_number = random.randint(1, 20)
+print("Random number             :", random_number)
+print("Square root of random no. :", math.sqrt(random_number))
+print("Factorial of random no.   :", math.factorial(random_number))
+print("Square of random no.      :", random_number ** 2)
