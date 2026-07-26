7fa6195f921257e1b4dacdd53e8b24a4c9d8d085
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-26 18:40:21 +0500

    feat : complete practice 01 importing math module

---
+++ cisco-python-essentials-2/module_01_modules/sec_1_1_practice/practice_01_import_math_module.py
@@ -0,0 +1,6 @@
+import math
+
+print("PI                 :", math.pi)
+print("Euler number       :", math.e)
+print("Square root of 144 :", math.sqrt(144))
+print("Factorial of 5     :", math.factorial(5))
