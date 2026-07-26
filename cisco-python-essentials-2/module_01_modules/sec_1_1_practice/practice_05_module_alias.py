d901312a6559950e5966e576425ed443fbc26a1e
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-26 18:44:49 +0500

    feat : complete practice 05 module aliasing

---
+++ cisco-python-essentials-2/module_01_modules/sec_1_1_practice/practice_05_module_alias.py
@@ -0,0 +1,7 @@
+import math as m
+
+print("PI :", m.pi)
+print("Square root of 81          :", m.sqrt(81))
+print("2 raised to the power of 3 :", 2 ** 3)
+print("ceil(5.4)                  :", m.ceil(5.4))
+print("floor(5.4)                 :", m.floor(5.4))
