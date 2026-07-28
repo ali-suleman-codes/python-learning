b5452cfb852d3493ca12a243d85a17d5be8b4acc
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-28 22:50:41 +0500

    fix: handle undefined tangent using math.isclose

--- cisco-python-essentials-2/module_01_modules/sec_1_2_practice/practice_02_trigonometric_functions.py
+++ cisco-python-essentials-2/module_01_modules/sec_1_2_practice/practice_02_trigonometric_functions.py
@@ -2,7 +2,12 @@ import math
 
 angle_in_degrees = float(input("Enter an angle in degrees : "))
 angle_in_radians = math.radians(angle_in_degrees)
+
 print("Angle in radians :", angle_in_radians)
-print("sine(", angle_in_degrees, "°) : ", math.sin(angle_in_radians), sep= "")
-print("cos(", angle_in_degrees, "°)  : ", math.cos(angle_in_radians), sep= "")
-print("tan(", angle_in_degrees, "°)  : ", math.tan(angle_in_radians), sep= "")
+
+print(f"sin({angle_in_degrees})° : {math.sin(angle_in_radians)}")
+print(f"cos({angle_in_degrees})° : {math.cos(angle_in_radians)}")
+if math.isclose(math.cos(angle_in_radians), 0.0, abs_tol= 1e-12):
+    print(f"tan({angle_in_degrees})° : undefined")
+else :
+    print(f"tan({angle_in_degrees})° : {math.tan(angle_in_radians)}")
