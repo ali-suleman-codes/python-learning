b8d9f73ff9fb33226e73172fec3d2e0df3e991cd
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-07-30 00:00:48 +0500

    feat: complete practice 04 using exponential and logarithmic functions

---
+++ cisco-python-essentials-2/module_01_modules/sec_1_2_practice/practice_04_exponential_and_logarithmic_functions.py
@@ -0,0 +1,9 @@
+import math
+
+print("e            :", math.e)
+print("exp(2)       :", math.exp(2))
+print("log(10)      :", math.log(10))
+print("log(27, 3)   :", math.log(27, 3))
+print("log10(10000) :", math.log10(10000))
+print("log2(8)      :", math.log2(8))
+print("pow(2, 3)    :", pow(2, 3))
