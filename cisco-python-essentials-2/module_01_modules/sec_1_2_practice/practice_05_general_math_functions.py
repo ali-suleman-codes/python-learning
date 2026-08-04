c8595fbbf33450c5e307ed16d948ce360dadda66
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-08-04 12:10:38 +0500

    feat : complete practice 05 using general math functions

---
+++ cisco-python-essentials-2/module_01_modules/sec_1_2_practice/practice_05_general_math_functions.py
@@ -0,0 +1,19 @@
+import math
+
+num = 64.4
+print(f"ceil({num})   : {math.ceil(num)}")
+
+num = -52.2
+print(f"floor({num}) : {math.floor(num)}")
+
+num = 324.4
+print(f"trunc({num}) : {math.trunc(num)}")
+
+num = 4
+print(f"factorial({num}) : {math.factorial(num)}")
+
+side_1 = 3
+side_2 = 4
+
+
+print(f"hypot({side_1}, {side_2})  : {math.hypot(side_1, side_2)}")
