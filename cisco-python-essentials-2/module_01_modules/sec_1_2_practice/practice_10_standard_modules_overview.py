320e7927e1620458eef1622b03ae295127542dc2
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-08-04 12:15:47 +0500

    feat : complete practice 10 reviewing standard modules

---
+++ cisco-python-essentials-2/module_01_modules/sec_1_2_practice/practice_10_standard_modules_overview.py
@@ -0,0 +1,7 @@
+from math import log2
+from random import randint
+from platform import system
+
+print("log2(16)         :", log2(16))
+print("Random value     :", randint(5, 10))
+print("Operating System :", system())
