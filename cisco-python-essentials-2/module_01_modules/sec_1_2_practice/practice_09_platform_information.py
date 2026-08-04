ae5ad1763285f57adfb8dde66411a2a703419e66
Muhammad Ali Suleman Rajpoot <alisuleman.cs@gmail.com>
2026-08-04 12:14:55 +0500

    feat : complete practice 09 retrieving platform information

---
+++ cisco-python-essentials-2/module_01_modules/sec_1_2_practice/practice_09_platform_information.py
@@ -0,0 +1,9 @@
+import platform
+
+print("Platform             :", platform.platform())
+print("Machine              :", platform.machine())
+print("Processor            :", platform.processor())
+print("System               :", platform.system())
+print("Version              :", platform.version())
+print("Implementation       :", platform.python_implementation())
+print("Python Version Tuple :", platform.python_version_tuple())
